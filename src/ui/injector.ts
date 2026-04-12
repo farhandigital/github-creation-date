811228466b7624f3772c2ef5e2540b893cbe0ebb
Invictus Navarchus <githubgpg.lumpiness807@passmail.com>
2026-04-12 18:51:11 +0700

    uiux: use calendar svg from lucide

--- src/ui/injector.ts
+++ src/ui/injector.ts
@@ -13,7 +13,8 @@ function createCreationDateElement(creationDate: string) {
 
     const creationDateElement = document.createElement('p');
     creationDateElement.classList.add('Link', 'Link--muted');
-    creationDateElement.textContent = `🗓️ ${formatDate(new Date(creationDate))}`;
+    const svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-icon lucide-calendar"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/></svg>`;
+    creationDateElement.innerHTML = `${svgString} ${formatDate(new Date(creationDate))}`;
     creationDateElement.dataset.creationDate = "creation-date-element";
     container.appendChild(creationDateElement);
     return container;
