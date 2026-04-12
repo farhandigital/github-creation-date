0397c4210cce0b8d08bf429d404e5a00a63fe6cf
Invictus Navarchus <githubgpg.lumpiness807@passmail.com>
2026-04-12 18:48:00 +0700

    fix: add simple mutex to prevent double execution from race condtiion

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,31 @@ import { getCreationDate } from "./adapters/getCreationDate";
 import { injectCreationDate, isAlreadyInjected } from "./ui/injector";
 import { extractUsernameAndRepo } from "./utils/url";
 
-async function main() {
-    const hostname = window.location.hostname;
-    if (hostname !== 'github.com') return;
-    if (isAlreadyInjected()) {
-        console.log('Creation date element already exists. Skipping injection.');
-        return;
-    }
-    const url = window.location.href;
-    const repoInfo = extractUsernameAndRepo(url);
-    if (!repoInfo) return;
-    
-    const { username, repo } = repoInfo;
-    const creationDate = await getCreationDate(username, repo);
-    injectCreationDate(creationDate);
+let isRunning = false;
 
-    console.log(`Repository ${username}/${repo} was created on: ${creationDate}`);
+async function main() {
+    if (isRunning) return;
+    isRunning = true;
+
+    try {
+        const hostname = window.location.hostname;
+        if (hostname !== 'github.com') return;
+        if (isAlreadyInjected()) {
+            console.log('Creation date element already exists. Skipping injection.');
+            return;
+        }
+        const url = window.location.href;
+        const repoInfo = extractUsernameAndRepo(url);
+        if (!repoInfo) return;
+        
+        const { username, repo } = repoInfo;
+        const creationDate = await getCreationDate(username, repo);
+        injectCreationDate(creationDate);
+    
+        console.log(`Repository ${username}/${repo} was created on: ${creationDate}`);
+    } finally {
+        isRunning = false;
+    }
 }
 
 setInterval(main, 300);
